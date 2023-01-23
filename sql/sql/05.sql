78d0eaeb4010fc0da431ba46a14c6fe72543e74e
Cam Webb <cw@camwebb.info>
2023-01-23 10:20:11 -0900

    auto

--- sql/sql/05.sql
+++ sql/sql/05.sql
@@ -7,4 +7,7 @@ WHERE flat.collection_object_id = flat_edit_history.collection_object_id
 ORDER BY edit_date DESC
 LIMIT 30
 
--- ALA users: steffi, camwebb, sustina, bjheitz
+-- ALA users: steffi, camwebb, susitna, bjheitz
+-- other possible filters:
+--   edited_table IN ('locality', 'identification', 'specimen_event') 
+--   edit_date >= '2022-11-01'
