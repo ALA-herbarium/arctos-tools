80094adb8d9d438776557d13912c45ca0dda371e
Cam Webb <cw@camwebb.info>
2023-02-02 16:32:50 -0900

    Updated export SQL

--- sql/sql/27.sql
+++ sql/sql/27.sql
@@ -66,15 +66,22 @@ SELECT
     '([^ ]+) +([^ ]+)', '\2, \1')
     AS "Collector",
   -- 19 - Collection # (direct)  ** edit **
-  CASE WHEN othercatalognumbers ~ 'field number='
+  CASE
+    WHEN othercatalognumbers ~ 'collector number='
+    THEN REGEXP_REPLACE(othercatalognumbers,
+      '^.*collector number=([^,]+).*$', '\1')
+    WHEN othercatalognumbers ~ 'field number='
     THEN REGEXP_REPLACE(othercatalognumbers,
       '^.*field number=([^,]+).*$', '\1')
+    WHEN othercatalognumbers ~ 'other identifier='
+    THEN REGEXP_REPLACE(othercatalognumbers,
+      '^.*other identifier=([^,]+).*$', '\1')
     ELSE NULL END
   AS "Collection #",
   -- 20 - Collection Date (transform)
-  TO_CHAR(ended_date::date, 'mm/dd/YYYY') AS "Collection Date",
-  -- 21 - Condition (fixed)  **/** use real data
-  'COM/GD' AS "Condition",
+  TO_CHAR(ended_date::date, 'FMMM/FMDD/YYYY') AS "Collection Date",
+  -- 21 - Condition (fixed)  
+  (partdetail::json->>0)::json->>'cd' AS "Condition",
   -- 22 - Condition Desc (fixed)
   -- '' AS "Condition Desc",
   -- 23 - Study # (fixed)
@@ -87,95 +94,99 @@ SELECT
     ELSE NULL END
     ) AS "Other Numbers",
   -- 25 - Cataloger (transform)  ** only in New records **
-  UPPER(REGEXP_REPLACE(
+  REGEXP_REPLACE(
     REGEXP_REPLACE(
       REGEXP_REPLACE(enteredby, ',.*',''),
     '[A-Z]\.',''),
-    '([^ ]+) +([^ ]+)', '\2, \1')) AS "Cataloger",
+    '([^ ]+) +([^ ]+)', '\2, \1') AS "Cataloger",
   -- 26 - Catalog Date (transform)   ** only in New records **
-  TO_CHAR(entereddate::date, 'mm/dd/YYYY') AS "Catalog Date",
+  TO_CHAR(entereddate::date, 'FMMM/FMDD/YYYY') AS "Catalog Date",
   -- 27 - Identified By (direct)
-  UPPER(REGEXP_REPLACE(
+  REGEXP_REPLACE(
     REGEXP_REPLACE(
       REGEXP_REPLACE(identifiedby, ',.*',''),
     '[A-Z]\.',''),
-    '([^ ]+) +([^ ]+)', '\2, \1')) AS "Identified By",
+    '([^ ]+) +([^ ]+)', '\2, \1') AS "Identified By",
   -- 28 - Ident Date (transform)
-  TO_CHAR(date_made_date::date, 'mm/dd/YYYY') AS "Ident Date",
+  TO_CHAR(date_made_date::date, 'FMMM/FMDD/YYYY') AS "Ident Date",
   -- 29 - Repro Method (fixed)
   -- '' AS "Repro Method",
-  -- 30 - Locality (direct)  ** ASK **
+  -- 30 - Locality (direct)
   verbatim_locality AS "Locality",
   -- 31 - Unit (fixed)
   -- '' AS "Unit",
   -- 32 - State (fixed)
   -- 'AK' AS "State",
-  -- 33 - Reference Datum (transform)   ** NOT FOR NOW **
-  CASE
-    WHEN datum = 'World Geodetic System 1984' THEN 'WGS 84'
-    WHEN datum = 'North American Datum 1927' THEN 'NAD 27'
-    WHEN datum = 'North American Datum 1983' THEN 'NAD 83'
-    ELSE NULL END
-  AS "Reference Datum",
+  -- 33 - Reference Datum (transform)   ** NOT FOR NOW, FIX LATER **
+  -- CASE
+  --   WHEN datum = 'World Geodetic System 1984' THEN 'WGS 84'
+  --   WHEN datum = 'North American Datum 1927' THEN 'NAD 27'
+  --   WHEN datum = 'North American Datum 1983' THEN 'NAD 83'
+  --   ELSE NULL END
+  -- AS "Reference Datum",
   -- 34 - Watrbody/Drain:Waterbody (fixed)
   -- '' AS "Watrbody/Drain:Waterbody",
   -- 35 - Watrbody/Drain:Drainage (fixed)
-  '' AS "Watrbody/Drain:Drainage",
+  -- '' AS "Watrbody/Drain:Drainage",
   -- 36 - UTM Z/E/N (fixed)
-  '' AS "UTM Z/E/N",
+  -- '' AS "UTM Z/E/N",
   -- 37 - Lat LongN/W (combine)
-  CONCAT_WS('', 'N', dec_lat, '/W',  REPLACE(dec_long::varchar,'-',''))
+  -- CONCAT_WS('', 'N', dec_lat, '/W',  REPLACE(dec_long::varchar,'-',''))
+  CONCAT_WS('', dec_lat, ',', dec_long)
     AS "Lat LongN/W",
   -- 38 - Elevation (transform)
-  CONCAT_WS('', max_elev_in_m, ' m.') AS "Elevation",
+  CASE WHEN max_elev_in_m IS NOT NULL
+    THEN CONCAT_WS('', max_elev_in_m, ' m')
+  ELSE NULL END
+    AS "Elevation",
   -- 39 - Depth (fixed)
-  '' AS "Depth",
+  -- '' AS "Depth",
   -- 40 - Depos Environ (fixed)
-  '' AS "Depos Environ",
+  -- '' AS "Depos Environ",
   -- 41 - Habitat/Comm (fixed)
-  '' AS "Habitat/Comm",
+  -- '' AS "Habitat/Comm",
   -- 42 - Habitat (direct)
   habitat AS "Habitat",
   -- 43 - Slope (fixed)
-  '' AS "Slope",
+  -- '' AS "Slope",
   -- 44 - Aspect (fixed)
-  '' AS "Aspect",
+  -- '' AS "Aspect",
   -- 45 - Soil Type (fixed)
-  '' AS "Soil Type",
+  -- '' AS "Soil Type",
   -- 46 - For/Per/Sub (fixed)
-  '' AS "For/Per/Sub",
+  -- '' AS "For/Per/Sub",
   -- 47 - Assoc Spec (fixed)
-  '' AS "Assoc Spec",
+  -- '' AS "Assoc Spec",
   -- 48 - Type Specimen (fixed)
   typestatus AS "Type Specimen",
   -- 49 - Threat/Endang (fixed)
-  '' AS "Threat/Endang",
+  -- '' AS "Threat/Endang",
   -- 50 - T/E Date (fixed)
-  '' AS "T/E Date",
+  -- '' AS "T/E Date",
   -- 51 - Rare (fixed)
-  '' AS "Rare",
+  -- '' AS "Rare",
   -- 52 - Exotic/Native (fixed)
-  '' AS "Exotic/Native",
+  -- '' AS "Exotic/Native",
   -- 53 - Age (fixed)
-  '' AS "Age",
+  -- '' AS "Age",
   -- 54 - Sex (fixed)
-  '' AS "Sex",
+  -- '' AS "Sex",
   -- 55 - Notes (fixed)
-  '' AS "Notes",
+  -- '' AS "Notes",
   -- 56 - Field Season
-  TO_CHAR(ended_date::date, 'YYYY') AS "Field Season",
+  TO_CHAR(ended_date::date, 'YYYY') AS "Field Season"
   -- 57 - Ctrl Prop (fixed)
-  'N' AS "Ctrl Prop",
+  -- 'N' AS "Ctrl Prop",
   -- 58 - Location (fixed)
-  'UAMN - ALA HERBARIUM' AS "Location",
+  -- 'UAMN - ALA HERBARIUM' AS "Location",
   -- 59 - Object Status (fixed)
-  'LOAN OUT – NON-NPS – NON-FEDERAL' AS "Object Status",
+  -- 'LOAN OUT – NON-NPS – NON-FEDERAL' AS "Object Status",
   -- 60 - Status Date (fixed)
-  '' AS "Status Date",
+  -- '' AS "Status Date",
   -- 61 - Catalog Folder (fixed)
-  'N' AS "Catalog Folder",
+  -- 'N' AS "Catalog Folder",
   -- 62 - Maint. Cycle (fixed)
-  '' AS "Maint. Cycle"
+  -- '' AS "Maint. Cycle"
 FROM flat
-WHERE guid IN ('UAM:Herb:243406', 'UAM:Herb:244304', 'UAM:Herb:141482', 'UAM:Herb:147801', 'UAM:Herb:147808', 'UAM:Herb:148352', 'UAM:Herb:148354', 'UAM:Herb:148428', 'UAM:Herb:39427', 'UAM:Herb:68501', 'UAM:Ento:473386')
+WHERE guid IN ('UAM:Herb:243406', 'UAM:Herb:244304', 'UAM:Herb:141482', 'UAM:Herb:147801', 'UAM:Herb:147808', 'UAM:Herb:148352', 'UAM:Herb:148354', 'UAM:Herb:148428', 'UAM:Herb:39427', 'UAM:Herb:68501')
 ORDER BY guid ;
