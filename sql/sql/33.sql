ad6e9ace1e15f17e722d850d5cad30884a9e42c3
Cam Webb <cw@camwebb.info>
2023-05-09 14:28:51 -0800

    fixed duplicate SQL

--- sql/sql/33.sql
+++ sql/sql/33.sql
@@ -1,35 +1,39 @@
 -- Duplicate entries for single specimen, by ALAAC, showing some fields
 
-SELECT A.alaac, n, orialaac, bc, guid, spec_locality, collectors, scientific_name
-FROM (
-  SELECT * FROM (
-    SELECT 
-        REGEXP_REPLACE(othercatalognumbers,
-          '^.*ALAAC=([ABLV]?)(0*)([1-9][0-9]*).*$', '\1\3') AS alaac,
-      COUNT(*) as n
-      FROM flat
-      WHERE guid ~ 'UAMb?:(Alg|Herb)' AND
-      othercatalognumbers ~ 'ALAAC'
-      GROUP BY alaac
-   ) AS A0
-   WHERE A0.n > 1
-) AS A
-LEFT JOIN (
 SELECT
-  guid,
-  (partdetail::json->>0)::json->>'bc' AS bc,
+  A.*,
+  B.alaac_arctos,
+  flat.guid,
+  (partdetail::json->>0)::json->>'bc' AS barcode,
+  higher_geog,
   spec_locality,
   collectors,
-  scientific_name,
-  REGEXP_REPLACE(othercatalognumbers,
-        '^.*ALAAC=([ABLV]?)(0*)([1-9][0-9]*).*$', '\1\3') AS alaac2,
-  REGEXP_REPLACE(othercatalognumbers,
-        '^.*ALAAC=([ABLV]?)(0*)([1-9][0-9]*).*$', '\1\2\3') AS orialaac
-  FROM flat
-  WHERE guid ~ 'UAMb?:(Alg|Herb)' AND
-  othercatalognumbers ~ 'ALAAC' 
-  AND spec_locality ~ '^[Uu]nknown$'
+  began_date,
+  scientific_name
+FROM (
+  SELECT * FROM (
+    SELECT
+      REGEXP_REPLACE(display_value,
+        '^([ABLV]?)(0*)([1-9][0-9]*)$', '\1\3') AS alaac_clean,
+      COUNT(*) AS n
+    FROM coll_obj_other_id_num
+    WHERE other_id_type = 'ALAAC'
+    GROUP BY alaac_clean
+  ) AS A0
+  WHERE A0.n > 1
+) AS A
+LEFT JOIN (
+  SELECT 
+    collection_object_id AS coi, 
+    REGEXP_REPLACE(display_value,
+      '^([ABLV]?)(0*)([1-9][0-9]*)$', '\1\3') AS alaac_clean2,
+    display_value AS alaac_arctos
+  FROM coll_obj_other_id_num
+  WHERE other_id_type = 'ALAAC'
 ) AS B
-ON A.alaac = B.alaac2
-ORDER BY A.alaac;
+ON A.alaac_clean = B.alaac_clean2
+LEFT JOIN flat
+ON B.coi = flat.collection_object_id
+ORDER BY A.alaac_clean, length(guid), guid
+;
 
