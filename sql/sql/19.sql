6e4e099c820d4e0c5d306b33a96f3279c534ddcb
Cam Webb <cw@camwebb.info>
2023-06-08 18:22:57 -0800

    auto

--- sql/sql/19.sql
+++ sql/sql/19.sql
@@ -1,6 +1,14 @@
 -- Specimens, photos but no transcribed data, return GUID and Media URL
 
-SELECT flat.guid, media_flat.media_uri
+SELECT
+ flat.guid,
+ media_flat.media_uri,
+ CASE WHEN othercatalognumbers ~ 'ALAAC'
+   THEN REGEXP_REPLACE(othercatalognumbers,
+      '^.*ALAAC=([ABLV]?[0-9]+).*$', '\1')
+   ELSE NULL END
+ AS ALAAC,
+ (partdetail::json->>0)::json->>'bc' AS barcode
 FROM flat LEFT JOIN (
 SELECT r.related_primary_key AS co, MAX(m.media_id) AS mi
   FROM media_relations AS r, media_flat AS m
