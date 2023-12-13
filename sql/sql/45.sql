c1209abea8d071baf7672d4d996c625001158812
Cam Webb <cw@camwebb.info>
2023-12-12 16:56:39 -0900

    auto

--- sql/sql/45.sql
+++ sql/sql/45.sql
@@ -2,10 +2,80 @@
 
 SELECT taxon, photo, locninfo, geo, count(*) from (
   SELECT
-    CASE WHEN phylum ~ '^Bryophyta' THEN 'moss'
-      -- assume very few non-lich fungi
-      WHEN    kingdom ~ '^Fungi'    THEN 'lichen'
-      ELSE NULL END
+    -- NCBI families under Bryophyta
+    CASE WHEN phylum = 'Bryophyta' OR family IN
+('Amblystegiaceae', 'Ambuchananiaceae', 'Andreaeaceae',
+'Andreaeobryaceae', 'Anomodontaceae', 'Aongstroemiaceae',
+'Archidiaceae', 'Aulacomniaceae', 'Bartramiaceae', 'Brachytheciaceae',
+'Braithwaiteaceae', 'Bruchiaceae', 'Bryaceae', 'Bryobartramiaceae',
+'Bryoxiphiaceae', 'Buxbaumiaceae', 'Callicladiaceae',
+'Calliergonaceae', 'Calymperaceae', 'Catagoniaceae', 'Catoscopiaceae',
+'Climaciaceae', 'Cryphaeaceae', 'Daltoniaceae', 'Dicranaceae',
+'Dicranellopsidaceae', 'Diphysciaceae', 'Disceliaceae',
+'Ditrichaceae', 'Drummondiaceae', 'Echinodiaceae', 'Encalyptaceae',
+'Entodontaceae', 'Erpodiaceae', 'Eustichiaceae', 'Fabroniaceae',
+'Fissidentaceae', 'Flatbergiaceae', 'Flexitrichaceae',
+'Fontinalaceae', 'Funariaceae', 'Gigaspermaceae', 'Grimmiaceae',
+'Hedwigiaceae', 'Helicophyllaceae', 'Helodiaceae', 'Hookeriaceae',
+'Hylocomiaceae', 'Hypnaceae', 'Hypnodendraceae', 'Hypodontiaceae',
+'Hypopterygiaceae', 'Jocheniaceae', 'Lembophyllaceae',
+'Leptodontaceae', 'Leptostomataceae', 'Lepyrodontaceae', 'Leskeaceae',
+'Leucobryaceae', 'Leucodontaceae', 'Leucomiaceae', 'Meesiaceae',
+'Meteoriaceae', 'Micromitriaceae', 'Mitteniaceae', 'Miyabeaceae',
+'Mniaceae', 'Myriniaceae', 'Myuriaceae', 'Neckeraceae',
+'Oedipodiaceae', 'Orthodontiaceae', 'Orthorrhynchiaceae',
+'Orthostichellaceae', 'Orthotrichaceae', 'Phyllodrepaniaceae',
+'Phyllogoniaceae', 'Pilotrichaceae', 'Plagiotheciaceae',
+'Pleurophascaceae', 'Pleuroziopsidaceae', 'Polytrichaceae',
+'Pottiaceae', 'Prionodontaceae', 'Pseudoditrichaceae',
+'Pterigynandraceae', 'Pterobryaceae', 'Pterobryellaceae',
+'Ptychomitriaceae', 'Ptychomniaceae', 'Pulchrinodaceae',
+'Pylaisiaceae', 'Pylaisiadelphaceae', 'Racopilaceae',
+'Regmatodontaceae', 'Rhabdoweisiaceae', 'Rhachitheciaceae',
+'Rhacocarpaceae', 'Rhizogemmaceae', 'Rhizogoniaceae', 'Rhytidiaceae',
+'Rigodiaceae', 'Roellobryaceae', 'Ruficaulaceae', 'Rutenbergiaceae',
+'Saelaniaceae', 'Saulomataceae', 'Schimperobryaceae',
+'Schistostegaceae', 'Scorpidiaceae', 'Scouleriaceae', 'Seligeriaceae',
+'Sematophyllaceae', 'Serpotortellaceae', 'Sphagnaceae',
+'Splachnaceae', 'Stereodontaceae', 'Stereophyllaceae',
+'Symphyodontaceae', 'Takakiaceae', 'Taxiphyllaceae', 'Tetraphidaceae',
+'Theliaceae', 'Thuidiaceae', 'Timmiaceae', 'Trachylomataceae') THEN 'moss'
+      -- from doi:10.1639/0007-2745-119.4.361
+      WHEN    family in
+('Acarosporaceae', 'Andreiomycetaceae', 'Antennulariellaceae',
+'Aphanopsidaceae', 'Arctomiaceae', 'Arthoniaceae', 'Arthopyreniaceae',
+'Arthrorhaphidaceae', 'Atheliaceae', 'Baeomycetaceae',
+'Biatorellaceae', 'Brigantieaceae', 'Caliciaceae', 'Cameroniaceae',
+'Candelariaceae', 'Carbonicolaceae', 'Catillariaceae',
+'Celotheliaceae', 'Chrysotrichaceae', 'Cladoniaceae', 'Clavulinaceae',
+'Coccocarpiaceae', 'Coccotremataceae', 'Coenogoniaceae',
+'Collemataceae', 'Coniocybaceae', 'Corticiaceae', 'Cystocoleaceae',
+'Dacampiaceae', 'Dactylosporaceae', 'Elixiaceae', 'Fuscideaceae',
+'Gloeoheppiaceae', 'Gomphillaceae', 'Graphidaceae', 'Gyalectaceae',
+'Gypsoplacaceae', 'Haematommataceae', 'Helocarpaceae',
+'Hygrophoraceae', 'Hymeneliaceae', 'Icmadophilaceae', 'Incertae',
+'Koerberiaceae', 'Lecanographaceae', 'Lecanoraceae', 'Lecideaceae',
+'Lepidostromataceae', 'Leprocaulaceae', 'Letrouitiaceae',
+'Lichinaceae', 'Lobariaceae', 'Lopadiaceae', 'Lyrommataceae',
+'Malmideaceae', 'Massalongiaceae', 'Megalosporaceae', 'Megasporaceae',
+'Melaspileaceae', 'Microtheliopsidaceae', 'Miltideaceae',
+'Monoblastiaceae', 'Mycoporaceae', 'Mycosphaerellaceae',
+'Nephromataceae', 'Ochrolechiaceae', 'Opegraphaceae',
+'Ophioparmaceae', 'Pachyascaceae', 'Pannariaceae', 'Parmeliaceae',
+'Peltigeraceae', 'Peltulaceae', 'Pertusariaceae', 'Phlyctidaceae',
+'Physciaceae', 'Pilocarpaceae', 'Placynthiaceae', 'Porinaceae',
+'Protothelenellaceae', 'Psilolechiaceae', 'Psoraceae', 'Pycnoraceae',
+'Pyrenotrichaceae', 'Pyrenulaceae', 'Racodiaceae', 'Ramalinaceae',
+'Ramboldiaceae', 'Requienellaceae', 'Rhizocarpaceae', 'Roccellaceae',
+'Roccellographaceae', 'Ropalosporaceae', 'Sagiolechiaceae',
+'Sarrameanaceae', 'Schaereriaceae', 'Scoliciosporaceae',
+'Sphaerophoraceae', 'Sporastatiaceae', 'Stereocaulaceae',
+'Stictidaceae', 'Strangosporaceae', 'Strigulaceae', 'Teloschistaceae',
+'Tephromelataceae', 'Thelenellaceae', 'Thelocarpaceae',
+'Thrombiaceae', 'Trapeliaceae', 'Trypetheliaceae', 'Umbilicariaceae',
+'Vahliellaceae', 'Verrucariaceae', 'Vezdaeaceae', 'Xanthopyreniaceae',
+'Xylographaceae') THEN 'lichen'
+      ELSE 'other' END
       AS taxon,
     CASE WHEN imageurl IS NOT NULL THEN 'yes'
       ELSE 'no' END
@@ -21,9 +91,7 @@ SELECT taxon, photo, locninfo, geo, count(*) from (
     AS locninfo
   FROM flat
   WHERE
-    guid ~ 'UAMb:Herb' AND
-    ( phylum ~ '^Bryophyta' OR 
-      kingdom ~ '^Fungi' )
+    guid ~ 'UAMb:Herb' 
 ) AS A
 GROUP BY taxon, photo, locninfo, geo
 ORDER BY taxon, photo, locninfo, geo
